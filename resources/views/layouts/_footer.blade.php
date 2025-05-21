bb81c991a0a372e6abe45c35c4337dd0a3b961b7
Li Rundian <lirundian2010@gmail.com>
2025-05-21 18:05:05 +0900

    create-root-header-footer

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-start">
+            Design and Program by <a href="https://localhost:8000" target="_blank">Li Rundian</a>  <span
+                style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-end"><a href="mailto:lirundian2010@email.com">contact to us</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Homepage')
+
+@section('content')
+    <h1>Welcome to homepage</h1>
+@stop
