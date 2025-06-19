d042d8eaa99dabf761120dfa7b776bb3b87c430a
Li Rundian <lirundian2010@gmail.com>
2025-06-20 02:02:27 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/editor/css/simditor.css',
                 'resources/js/app.js',
+                'resources/editor/js/module.js',
+                'resources/editor/js/hotkeys.js',
+                'resources/editor/js/uploader.js',
+                'resources/editor/js/simditor.js',
             ],
             refresh: true,
         }),
