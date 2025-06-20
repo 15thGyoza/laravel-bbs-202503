67e8bb0e5496075e3a61ddd557abe738c53499e8
Li Rundian <lirundian2010@gmail.com>
2025-06-20 18:10:38 +0900

    debug-create

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,16 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/editor/css/simditor.css',
                 'resources/js/app.js',
-                'resources/editor/js/module.js',
-                'resources/editor/js/hotkeys.js',
-                'resources/editor/js/uploader.js',
-                'resources/editor/js/simditor.js',
             ],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true // 隐藏依赖项的警告
+            }
+        }
+    }
 });
